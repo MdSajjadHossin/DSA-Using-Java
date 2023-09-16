17fc994a57a6d1d310a48672b26e38c9cda8d359
Md Sajjad Hossin <iamsajjadhossin@gmail.com>
2023-09-16 14:16:46 +0600

    rotate array in place added

---
+++ ArrayRotateInPlace.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class ArrayRotateInPlace {
+
+    static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void reverse(int[] arr, int start, int end){
+        while(start < end){
+            swap(arr, start, end);
+                start++;
+                end--;
+        }
+    }
+
+    static void rotateInPlace(int[] arr, int k){
+        int n = arr.length;
+        k = k % n;
+        reverse(arr, 0, n-k-1);
+        reverse(arr, n-k, n-1);
+        reverse(arr, 0, n-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Element Number: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter "+n+" element in the array");
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter Rotation number: ");
+        int k = sc.nextInt();
+        rotateInPlace(arr, k);
+        System.out.println("Result after rotation");
+        for(int i = 0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
